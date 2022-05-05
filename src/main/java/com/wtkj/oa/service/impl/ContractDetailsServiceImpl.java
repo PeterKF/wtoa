6b00e09bd15a4be8fedf986b71d6795b2f858bad
PeterKF <35757468+PeterKF@users.noreply.github.com>
2022-05-05 19:53:10 +0800

    各级科技项目

--- src/main/java/com/wtkj/oa/service/impl/ContractDetailsServiceImpl.java
+++ src/main/java/com/wtkj/oa/service/impl/ContractDetailsServiceImpl.java
@@ -187,9 +187,11 @@ public class ContractDetailsServiceImpl implements IContractDetailsService {
 
         //默认为1
         if (c.getBusinessType().equals(2)) {
+            //各级科技项目
             if (c.getContractType().equals("3")) {
                 detail.setNumber(StrUtil.isNotEmpty(c.getServiceDetails().getPercent()) ?
                         Float.valueOf(c.getServiceDetails().getPercent()) / 100 : 1);
+                detail.setUnitFee(Integer.valueOf(c.getContractMatter()));
             }
         } else if (c.getBusinessType().equals(3)) {
             //企业研发费加计扣除
