061a2fe23373e8253f529b36e7df5d32396a1e8f
LAPTOP-2UFCMSOO\Peter <chenqiangjs@163.com>
2022-05-05 23:39:47 +0800

    用日志输出

--- src/main/java/com/wtkj/oa/App.java
+++ src/main/java/com/wtkj/oa/App.java
@@ -1,11 +1,13 @@
 package com.wtkj.oa;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Slf4j
 @EnableConfigurationProperties
 @SpringBootApplication
 @EnableSwagger2
@@ -13,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("server启动成功！");
+        log.info("项目开始启动");
         SpringApplication.run(App.class, args);
     }
 }
